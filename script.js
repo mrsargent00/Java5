a9fa500dbe4329a7099e33310e3fa5ff8d589259
Fluffy <mrsargent00@gmail.com>
2023-08-23 16:14:48 -0400

    added to JS

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var password = generatePassword(); 
+  passwordText.value = password; 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  function generatePassword() {
+    var length = 24; 
+    var charset = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%^&*"; // Character set for the password
+    var newPassword = "";
+
+    for (var i = 0; i < length; i++) {
+    var Index = Math.floor(Math.random() * charset.length);
+    newPassword += charset[Index];
+  }
+
+  return newPassword;
